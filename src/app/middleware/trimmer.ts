5e7239ced195d424fc6304023dcd4bb525c0746c
farhad <farhadfallahpour.eleceng@gmail.com>
2025-06-03 22:14:39 +0330

    fix(middleware): resolve bug in trimmer middlewar

--- src/app/middleware/trimmer.ts
+++ src/app/middleware/trimmer.ts
@@ -1,7 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 
 export default function Trimmer(req: Request, res: Response, next: NextFunction) {
-    if (req.method === 'POST') {
+    if (req.method === 'POST' && req.body) {
         for (const [key, value] of Object.entries(req.body)) {
             if (typeof (value) === 'string')
                 req.body[key] = value.trim();
